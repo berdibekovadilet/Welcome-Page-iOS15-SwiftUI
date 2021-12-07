75f4253cf90a7e1ccd10e40887da1a5ece56c4b4
Adilet <berdibekovadilet@icloud.com>
2021-12-07 11:52:59 +0300

    Initial Commit

---
+++ Welcome Page/Welcome_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_PageApp.swift
+//  Welcome Page
+//
+//  Created by Adilet Berdibekov on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
